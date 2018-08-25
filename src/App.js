9f22bdb838d1fd8225c16a7d54d69c16f1551a4e
Permadi Wibisono <mbapewe@gmail.com>
2018-08-26 01:51:42 +0700

    Init making Form for generate paginations

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import { Container, Col } from 'reactstrap';
+import { Container, Col, Form, FormGroup, Label, Input, Button, Row } from 'reactstrap';
 import logo from './logo.svg';
 import Paginations from './components/paginations';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      currentPage: 1,
+      lastPage: 4,
+      total: 200,
+      buttonsCount: 8,
+      perPage: 10
+    }
+  }
   render() {
     return (
       <div className="App">
@@ -13,21 +23,69 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <Container>
-          <Col>
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-          </Col>
+          <Row>
+            <Col xs={12} md={{size: 6, offset:3}}>
+              <Form className="myForm">
+                <Row>
+                  <Col xs={12} md={6}>
+                    <FormGroup>
+                      <Label>Current Page</Label>
+                      <Input name="currentPage" value={this.state.currentPage} type="number"/>
+                    </FormGroup>
+                  </Col>
+                  <Col xs={12} md={6}>
+                    <FormGroup>
+                      <Label>Last Page</Label>
+                      <Input name="lastPage" value={this.state.lastPage} disabled type="number"/>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs={12} md={6}>
+                    <FormGroup>
+                      <Label>Per Page</Label>
+                      <Input name="perPage" type="select" value={this.state.perPage}>
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                  <Col xs={12} md={6}>
+                    <FormGroup>
+                      <Label>Total</Label>
+                      <Input name="total" type="number" value={this.state.total}/>
+                    </FormGroup>
+                  </Col>
+                  <Col xs={12} md={6}>
+                    <FormGroup>
+                      <Label>Buttons Count</Label>
+                      <Input name="buttonCount" type="select" value={this.state.buttonCount}>
+                        <option value="10">5</option>
+                        <option value="25">8</option>
+                        <option value="50">10</option>
+                      </Input>
+                    </FormGroup>
+                  </Col>
+                </Row>
+                <FormGroup>
+                  <Button color="primary">Generate</Button>
+                </FormGroup>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+        <Container>
           <Col xs={12}>
             <Paginations
               size="sm"
-              lastPage={4}
-              perPage={10}
-              total={200}
-              buttonsCount={8}
-              currentPage={1}
+              lastPage={this.state.lastPage}
+              perPage={this.state.perPage}
+              total={this.state.total}
+              buttonsCount={this.state.buttonsCount}
+              currentPage={this.state.currentPage}
             />
-
           </Col>
         </Container>
       </div>
